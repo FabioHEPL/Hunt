b5a029fddc8b7e7dbd0f2c7b07cb4fc4dc1197f4
Thomas <Capitain.thomas@gmail.com>
2019-06-03 17:07:30 +0200

    PlayerGrow et PlayerThrow en dev

--- Assets/Scripts/PlayerGrow.cs
+++ Assets/Scripts/PlayerGrow.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class PlayerGrow : MonoBehaviour
 {
+    int numberOfGrow = 0;
+
     void Grow()
     {
-        
+        numberOfGrow++;
+        transform.localScale *= 1.2f;
+    }
+    private void Start()
+    {
+        GetComponent<ColliderManager>().onCollisionEvent += OnCollisionEvent;
+    }
+
+    private void OnCollisionEvent(object o, ColliderManager.onCollisionEventArgs args)
+    {
+        if (args.type == ColliderManager.colliderType.powerUp)
+        {
+            Grow();
+        }
     }
 }
--- Assets/Scripts/PlayerThrow.cs
+++ Assets/Scripts/PlayerThrow.cs
@@ -4,5 +4,34 @@ using UnityEngine;
 
 public class PlayerThrow : MonoBehaviour
 {
-    
+    bool fury;
+    bool canFury;
+
+    private void Start()
+    {
+        GetComponent.
+        GetComponent<ColliderManager>().onCollisionEvent += OnCollisionEvent;
+    }
+
+    private void OnCollisionEvent(object o, ColliderManager.onCollisionEventArgs args)
+    {
+        if (args.type == ColliderManager.colliderType.obstacle && )
+        {
+             
+        }
+    }
+
+    private void onPresseRageButton(object o, )
+    {
+        if (canFury)
+        {
+            Fury();
+        }
+    }
+
+    private void Fury()
+    {
+        fury = true;
+
+    }
 }
