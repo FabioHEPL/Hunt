1f25f54adae19f1dc8de81cd061836be2551ec8f
Thomas <Capitain.thomas@gmail.com>
2019-06-03 15:31:08 +0200

    création de l'évenment coin

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject coin;
+
+    public event Action<string> Collided;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Collided?.Invoke(collision.gameObject.tag);
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
