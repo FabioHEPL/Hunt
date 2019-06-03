05e04b993d245cd690dc824fcea70e6bed57fa05
PetitT <xcthib@hotmail.com>
2019-06-03 15:23:16 +0200

    DogGameOverObstacleDestroyWall DogGameOver

--- Assets/Scripts/ObstacleDestroyWall.cs
+++ Assets/Scripts/ObstacleDestroyWall.cs
@@ -15,4 +15,10 @@ public class ObstacleDestroyWall : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
 }
