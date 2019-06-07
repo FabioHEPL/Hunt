4802a3db110f15c495e1c8b179f885d514452400
PetitT <xcthib@hotmail.com>
2019-06-07 12:11:30 +0200

    Spawn

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : Spawn
+{
+
+    void Start()
+    {
+        minTimeToSpawn = DataContainer.singleton.data.spawn.minTimeToSpawnObstacle;
+        maxTimeToSpawn = DataContainer.singleton.data.spawn.maxTimeToSpawnObstacle;
+        StartCoroutine("Spawner");
+    }
+
+    public override IEnumerator Spawner ()
+    {
+        float timeToSpawn = Random.Range(minTimeToSpawn, maxTimeToSpawn);
+        int obstacleToSpawn = Random.Range(0, DataContainer.singleton.data.spawn.obstaclesPrefabs.Count);
+        spawn = DataContainer.singleton.data.spawn.obstaclesPrefabs[obstacleToSpawn];
+        yield return new WaitForSeconds(timeToSpawn);
+        Instantiate(spawn, transform.position, transform.rotation);
+        StartCoroutine("Spawner");
+    }
+    
+}
